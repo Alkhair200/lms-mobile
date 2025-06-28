4e155b8c212f17fd6b3896a78689f785f486e7b2
Alkhair <alhir1996@gmail.com>
2025-06-28 18:19:23 +0200

    first commit

---
+++ android/app/src/main/kotlin/com/babia/to/MainActivity.kt
@@ -0,0 +1,9 @@
+package com.babia.to
+
+import io.flutter.embedding.android.FlutterActivity
+import com.facebook.FacebookSdk;
+import com.facebook.appevents.AppEventsLogger;
+
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/common/utils/constants.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class Constants {
+  
+  
+  static const dommain = 'https://app.babia.to';
+  static const baseUrl = '$dommain/api/development/';
+  static const apiKey = 'linhcute';
+  static const scheme = 'academyapp';
+  
+  static final RouteObserver<ModalRoute<void>> singleCourseRouteObserver = RouteObserver<ModalRoute<void>>();
+  static final RouteObserver<ModalRoute<void>> contentRouteObserver = RouteObserver<ModalRoute<void>>();
+
+}
